420eae03e609aff6f1421454a07bf86ec9c358a7
shri234 <43914954+shri234@users.noreply.github.com>
2023-03-05 10:55:42 +0530

    Update student.controller.js

--- studentelectiveform/student.controller.js
+++ studentelectiveform/student.controller.js
@@ -5,6 +5,6 @@ router.get("/get", Controller.get_student);
 router.post("/create", Controller.create_student);
 
 router.post("/create/elective", Controller.create_elective);
-router.get("/get/elective", Controller.get_elective);
+router.get("/get/elective/1", Controller.get_elective);
 router.post("/create/1", Controller.getone_elective);
 module.exports = router;
